a3c7409c88d8b006624f2c374bd29083937c3650
davidxv15 <davidxvaz@gmail.com>
2024-07-20 22:14:34 -0700

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import App from './App';
+import Home from './pages/Home';
+import ChatRoom from './pages/ChatRoom';
+import Login from './pages/Login';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="chat" element={<ChatRoom />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);

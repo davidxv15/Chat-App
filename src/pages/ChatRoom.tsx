7bae9339eb0ce2d795a8ff97e86ad801e50edf5c
davidxv15 <davidxvaz@gmail.com>
2024-08-03 23:40:47 -0700

    btn more blue

--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -78,7 +78,7 @@ const ChatRoom: React.FC = () => {
       />
       <button
         onClick={sendMessage}
-        className="mt-2 bg-blue-500 text-white p-2 rounded-md"
+        className="mt-2 bg-blue-600 text-white p-2 rounded-md"
       >
         Send
       </button>

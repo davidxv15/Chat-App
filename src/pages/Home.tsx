290ab4565113fd93ca8e97d35f9a47fadef90368
davidxv15 <davidxvaz@gmail.com>
2024-08-24 23:57:33 -0700

    chat btn edit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,14 +7,14 @@ const Home: React.FC = () => {
   const goToChat = () => {
     navigate('/chat');
   };
-  
+
   return (
     <div className="flex justify-center items-center min-h-screen bg-dkgray-100">
       <h1 className="text-3xl font-bold">Welcome to Real-Time Chat App</h1>
       <button onClick={goToChat}
       className="bg-blue-500 text-white px-4 py-2 rounded-md"
       >
-        Go Chat!
+        Chat!
       </button>
     </div>
   );

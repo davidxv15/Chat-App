d469b10545fc99b534c03b4297782809085edd46
davidxv15 <davidxvaz@gmail.com>
2024-08-24 23:58:04 -0700

    home page edit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,7 +10,7 @@ const Home: React.FC = () => {
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-dkgray-100">
-      <h1 className="text-3xl font-bold">Welcome to Real-Time Chat App</h1>
+      <h1 className="text-3xl font-bold">Welcome to Real-Time Chat</h1>
       <button onClick={goToChat}
       className="bg-blue-500 text-white px-4 py-2 rounded-md"
       >

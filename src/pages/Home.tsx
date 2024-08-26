32f3588910dec6a8b07bfb4c2ae816218fed07a8
davidxv15 <davidxvaz@gmail.com>
2024-08-25 22:28:14 -0700

    chat btn move

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,7 +9,7 @@ const Home: React.FC = () => {
   };
 
   return (
-    <div className="flex justify-center items-center min-h-screen bg-dkgray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
       <h1 className="text-3xl font-bold">Welcome to Real-Time Chat</h1>
       <button onClick={goToChat}
       className="mt-9 px-1 py-1 bg-blue-500 text-white rounded-md hover:bg-blue-800"

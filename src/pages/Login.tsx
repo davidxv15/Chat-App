dfec1d32db8e016f24ff66dcd8493669f9aa5d10
davidxv15 <davidxvaz@gmail.com>
2024-08-28 23:12:32 -0700

    add rememberMe variable

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -9,6 +9,7 @@ const Login: React.FC = () => {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
+  const [rememberMe, setRememberMe] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();

f9bf01f2eb4a1bfdddf18b1c5f2b6d2cd670114e
davidxv15 <davidxvaz@gmail.com>
2024-09-01 19:47:42 -0700

    ErrorPage h1 edit

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -10,7 +10,7 @@ const ErrorPage: React.FC = () => {
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-red-100">
-          <h1 className="text-4xl font-bold text-red-600 mb-4">Something went wrong!</h1>
+          <h1 className="text-4xl font-bold text-red-600 mb-4">Something went wrong.</h1>
           <p className="text-lg mb-4">We encountered an unexpected error. Please try again later.</p>
           <button
             onClick={goToHome}

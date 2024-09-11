88a1901253774223b69f63c7ab080111cd69904f
davidxv15 <davidxvaz@gmail.com>
2024-09-11 02:00:57 -0700

    strike 'chat' btn

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,13 +20,13 @@ const Home: React.FC = () => {
     <div className="flex flex-col items-center min-h-screen bg-gray-100 p-4 top-2">
       <h1 className="text-3xl font-bold">Welcome to Real-Time Chat</h1>
       <ChatroomSelector />
-      <button
+      {/* <button
         onClick={goToChat}
         ref={buttonRef}
         className="mt-9 px-1 py-1 bg-blue-500 text-white rounded-md hover:bg-blue-800"
       >
         Chat!
-      </button>
+      </button> */}
     </div>
   );
 };

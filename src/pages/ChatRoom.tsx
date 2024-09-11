2114781aa4f010c5c08af54de1a2cbd12bb8b4c0
davidxv15 <davidxvaz@gmail.com>
2024-09-11 02:35:49 -0700

    h1 edit

--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -174,7 +174,7 @@ const ChatRoom: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-200 dark:bg-gray-900 p-4">
       <h1 className="text-2xl font-bold dark:text-gray-400">
-        {roomName ? roomName.charAt(0).toUpperCase() + roomName.slice(1) : "General"} Chat
+       Welcome to {roomName ? roomName.charAt(0).toUpperCase() + roomName.slice(1) : "General"} Chat
         </h1>
 
       <div className="flex items-center space-x-2">

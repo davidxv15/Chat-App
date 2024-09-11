6fc13d25731ec485d0a85085c14f2714d00b21eb
davidxv15 <davidxvaz@gmail.com>
2024-09-10 21:26:02 -0700

    main.tsx add 'index' as parent for '/'

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { WebSocketProvider } from "./context/WebSocketContext";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
 import ChatRoom from "./pages/ChatRoom";
 import Login from "./pages/Login";
-import Register from './pages/Register'; 
-import NotFound from './pages/NotFound';
-import ErrorPage from "./pages/ErrorPage"
-import { AuthProvider } from './context/AuthContext';
+import Register from "./pages/Register";
+import NotFound from "./pages/NotFound";
+import ErrorPage from "./pages/ErrorPage";
+import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-      <Router>
-    <AuthProvider>
-    <WebSocketProvider>
-        <Routes>
-          <Route path="/" element={<App />}>
-          <Route index element={<Navigate to="/home" />} />
-          <Route path="/home" element={<Home />} />
-            <Route path="chat" element={<ChatRoom />} />
-            <Route path="login" element={<Login />} />
-            <Route path="/register" element={<Register />} /> 
-            <Route path="*" element={<NotFound />} /> 
-          </Route>
-          <Route path="/error" element={<ErrorPage />} /> 
-        </Routes>
-    </WebSocketProvider>
-    </AuthProvider>
-      </Router>
+    <Router>
+      <AuthProvider>
+        <WebSocketProvider>
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route index element={<Navigate to="/home" />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="chat" element={<ChatRoom />} />
+              <Route path="login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="*" element={<NotFound />} />
+            </Route>
+            <Route path="/error" element={<ErrorPage />} />
+          </Routes>
+        </WebSocketProvider>
+      </AuthProvider>
+    </Router>
   </React.StrictMode>
 );

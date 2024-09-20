1a744d59172061a986c59c4d5b65104b8296d357
davidxv15 <davidxvaz@gmail.com>
2024-09-19 23:44:39 -0700

    inline bg styles for register and login screens

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -60,11 +60,11 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold mb-4">Login</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900">
+      <h1 className="text-3xl font-bold mb-4 text-gray-400">Login</h1>
       <form
         onSubmit={handleSubmit}
-        className="bg-white p-4 rounded-lg shadow-md w-80"
+        className="bg-gray-300 p-4 rounded-lg shadow-md w-80"
       >
         <input
           type="text"

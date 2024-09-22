a484a8773bc95cde0f26edcbdd4dfa6b8d5be6cc
davidxv15 <davidxvaz@gmail.com>
2024-09-21 23:08:52 -0700

    register: bg-gray-900

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -42,7 +42,7 @@ const Register: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900">
       <h1 className="text-2xl font-bold mb-4">Register</h1>
       <form
         onSubmit={handleSubmit}

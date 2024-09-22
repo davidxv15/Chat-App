be3c2b5244e898d79a58af7d4e04f1f9d9e7e90d
davidxv15 <davidxvaz@gmail.com>
2024-09-21 23:12:04 -0700

    login h1: text-gray-200

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -61,7 +61,7 @@ const Login: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900">
-      <h1 className="text-3xl font-bold mb-4 text-gray-400">Login</h1>
+      <h1 className="text-3xl font-bold mb-4 text-gray-200">Login</h1>
       <form
         onSubmit={handleSubmit}
         className="bg-gray-300 p-4 rounded-lg shadow-md w-80"

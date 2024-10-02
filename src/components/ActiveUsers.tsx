cf87ad69a569ee4c05f4051af9474fd07941cd0f
davidxv15 <davidxvaz@gmail.com>
2024-10-02 00:12:37 -0700

    list visible edits

--- src/components/ActiveUsers.tsx
+++ src/components/ActiveUsers.tsx
@@ -23,8 +23,8 @@ const ActiveUsers: React.FC<{ room: string }> = ({ room }) => {
   }, [socket, room]);
 
   return (
-    <div className="active-users-list text-gray-100 bg-gray-800 px-2 py-2 absolute top-2 left-4">
-      <h3 className="Active-users">Active Users:</h3>
+    <div className="active-users-list text-gray-100 bg-gray-800 px-2 py-1 top-2 left-4">
+      <h3 className="Active-users">Online</h3>
       <ul className="">
         {activeUsers.map(user => (
           <li key={user} className="">{user}</li>

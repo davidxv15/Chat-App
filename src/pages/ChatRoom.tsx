e882422a6d1b5777817e950a861980aa7f074c7d
davidxv15 <davidxvaz@gmail.com>
2024-10-01 21:59:06 -0700

    Home btn: font-bold

--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -256,7 +256,7 @@ const ChatRoom: React.FC = () => {
 
       <button
         onClick={() => navigate("/home")}
-        className="bg-blue-500 text-white px-4 py-1 text-2xl rounded-md dark:bg-blue-800 dark:text-gray-400"
+        className="home-button font-bold bg-blue-500 text-white px-4 py-1 text-2xl rounded-md dark:bg-blue-800 dark:text-gray-400"
       >
         Home
       </button>

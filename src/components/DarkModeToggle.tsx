a4ab94a96354d64a6af22111b103dacfbeed3ff5
davidxv15 <davidxvaz@gmail.com>
2024-10-02 23:39:46 -0700

    darkMode: z-50

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -27,7 +27,7 @@ const DarkModeToggle: React.FC = () => {
   return (
     <button
       onClick={toggleDarkMode}
-      className="bg-gray-700 dark:bg-gray-600 text-white dark:text-gray-300 px-2 py-2 rounded absolute top-12 right-4"
+      className="bg-gray-700 dark:bg-gray-600 text-white dark:text-gray-300 px-2 py-2 rounded absolute top-12 right-4 z-50"
     >
       {darkMode ? (
         <SunIcon className="h-5 w-5 dark:text-yellow-400" /> // Show Sun icon in DARK mode

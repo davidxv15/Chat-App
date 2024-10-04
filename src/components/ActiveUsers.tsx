3b07ee55f2dced366222a04a3cc71d108214de1e
davidxv15 <davidxvaz@gmail.com>
2024-10-04 02:30:30 -0700

    list: transform, margins, pads

--- src/components/ActiveUsers.tsx
+++ src/components/ActiveUsers.tsx
@@ -23,7 +23,7 @@ const ActiveUsers: React.FC<{ room: string }> = ({ room }) => {
   }, [socket, room]);
 
   return (
-    <div className="active-users-list hover:scale-125 bg-gray-800 bg-opacity-80 text-white px-2 py-1 sticky  dark:bg-opacity-5 backdrop-blur-sm dark:text-yellow-100">
+    <div className="active-users-list hover:scale-125 bg-gray-800 bg-opacity-80 text-white px-2 py-0 sticky  dark:bg-opacity-5 backdrop-blur-sm dark:text-yellow-100">
       <h3 className="Active-users opacity-60">Online</h3>
       <ul className="">
         {activeUsers.map(user => (

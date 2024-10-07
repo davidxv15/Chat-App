ca95219a38bf616ad6134a123decff93c86a02a4
davidxv15 <davidxvaz@gmail.com>
2024-10-06 23:56:31 -0700

    bulletin: p: remove darkMode

--- src/components/Bulletin.tsx
+++ src/components/Bulletin.tsx
@@ -4,7 +4,7 @@ const Bulletin: React.FC = () => {
   return (
     <div className="bulletin rounded bg-gray-800 text-center px-14 py-4 mb-4 mt-4 border border-gray-300 dark:border-gray-800">
       <h2 className="text-2xl font-bold text-blue-600">Your Ad Here!</h2>
-      <p className="text-gray-500 dark:text-gray-400 p-2">
+      <p className="text-gray-400 p-2">
         Visit our sponsors! Advertise here!
       </p>
       <p className="contact">Contact </p>

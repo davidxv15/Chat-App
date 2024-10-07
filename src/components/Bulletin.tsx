10b54a5ef49f12a962b263d73f3a496fc94adc8f
davidxv15 <davidxvaz@gmail.com>
2024-10-06 23:51:40 -0700

    bulletin update

--- src/components/Bulletin.tsx
+++ src/components/Bulletin.tsx
@@ -5,7 +5,7 @@ const Bulletin: React.FC = () => {
     <div className="bulletin rounded bg-gray-700 text-center px-14 py-4 mb-4 mt-4 border-b border-gray-300 dark:border-gray-800">
       <h2 className="text-xl font-bold text-blue-600">Your Ad Here!</h2>
       <p className="text-gray-500 dark:text-gray-400 p-2">
-        Visit our sponsors! Advertise with us!
+        Visit our sponsors! Advertise here!
       </p>
       <p className="contact">Contact </p>
     </div>

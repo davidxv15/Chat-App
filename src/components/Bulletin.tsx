3b0761547184c2808ddb7add043a56c337e78056
davidxv15 <davidxvaz@gmail.com>
2024-10-06 23:52:49 -0700

    bulletin: bg-gray-800

--- src/components/Bulletin.tsx
+++ src/components/Bulletin.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Bulletin: React.FC = () => {
   return (
-    <div className="bulletin rounded bg-gray-700 text-center px-14 py-4 mb-4 mt-4 border-b border-gray-300 dark:border-gray-800">
+    <div className="bulletin rounded bg-gray-800 text-center px-14 py-4 mb-4 mt-4 border-b border-gray-300 dark:border-gray-800">
       <h2 className="text-xl font-bold text-blue-600">Your Ad Here!</h2>
       <p className="text-gray-500 dark:text-gray-400 p-2">
         Visit our sponsors! Advertise here!

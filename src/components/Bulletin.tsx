e64497f16c124f5551e5f9e208aa53b818dd310c
davidxv15 <davidxvaz@gmail.com>
2024-10-17 02:24:57 -0700

    Bulletin: JSON plug

--- src/components/Bulletin.tsx
+++ src/components/Bulletin.tsx
@@ -1,6 +1,26 @@
 import React from "react";
 
+interface Quote {
+  author: string;
+  text: string;
+}
+
 const Bulletin: React.FC = () => {
+  const [quote, setQuote] = useState<Quote | null>(null);
+
+  // Fetch the JSON file on component mount
+  useEffect(() => {
+    fetch('/quotes.json')
+      .then((response) => response.json())
+      .then((data) => {
+        const quotes = data.quotes;
+        const randomIndex = Math.floor(Math.random() * quotes.length);
+        setQuote(quotes[randomIndex]);
+      })
+      .catch((error) => console.error('Error loading quotes:', error));
+  }, []);
+
+
   return (
     <div
       className="bulletin rounded bg-gray-800 text-center px-14 py-4 mb-4 mt-4 border border-gray-300 dark:border-gray-800"

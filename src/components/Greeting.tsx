898d5c850e797b6067c7f45c623a1fe64653d6fa
davidxv15 <davidxvaz@gmail.com>
2024-10-26 22:21:01 -0700

    Greeting: bg-gray-700

--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -6,7 +6,7 @@ const Greeting: React.FC = () => {
 
   return (
     <div
-      className="bg-gray-800 bg-opacity-80 text-gray-300 dark:text-gray-400 dark:bg-gray-900"
+      className="bg-gray-700 bg-opacity-80 text-gray-300 dark:text-gray-400 dark:bg-gray-900"
       style={{
         position: "absolute",
         top: ".5rem",

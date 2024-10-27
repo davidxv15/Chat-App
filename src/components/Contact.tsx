128a5ec531eb16ff17fb00faedd3ed04f8ab630f
davidxv15 <davidxvaz@gmail.com>
2024-10-26 22:24:08 -0700

    Contact: p-1

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,7 +3,7 @@ import { FaLinkedin, FaEnvelope } from 'react-icons/fa';
 
 const Contact: React.FC = () => {
   return (
-    <div className="fixed bottom-0 left-0 w-full bg-transparent text-gray-200 p-4 shadow-lg opacity-80">
+    <div className="fixed bottom-0 left-0 w-full bg-transparent text-gray-200 p-1 shadow-lg opacity-80">
       <div className="max-w-screen-xl mx-auto flex justify-between items-center">
         <div className="text-lg font-semibold">
           {/* Optionally add a title or leave it blank for pure icons */}

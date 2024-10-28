a62711c77c3162262f0fcf3d90f44d8a2ee890e6
davidxv15 <davidxvaz@gmail.com>
2024-10-28 09:21:09 -0700

    Contact: icons

--- src/components/Bulletin.tsx
+++ src/components/Bulletin.tsx
@@ -23,7 +23,7 @@ const Bulletin: React.FC = () => {
 
   return (
     <div
-      className="bulletin rounded bg-gray-800 text-center px-14 py-4 mb-2 mt-4 border border-gray-600"
+      className="bulletin rounded bg-gray-800 text-center px-6 py-4 mb-2 mt-4 border border-gray-600"
       role="complementary"
       aria-labelledby="bulletin-title"
       aria-describedby="bulletin-description"

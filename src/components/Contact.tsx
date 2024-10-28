6bf5c0294d93260280487a01ef3072dfcd4d2450
davidxv15 <davidxvaz@gmail.com>
2024-10-27 22:28:29 -0700

    Icons only

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,7 +6,7 @@ const Contact: React.FC = () => {
     <div className="fixed bottom-0 left-0 w-full bg-transparent text-gray-200 p-2 shadow-lg opacity-80">
       <div className="max-w-screen-xl mx-2 flex justify-between items-center">
         <div className="text-lg">
-          Connect:
+          {/* Connect: */}
         </div>
         <div className="flex space-x-2 pl-2">
           {/* LinkedIn Icon */}

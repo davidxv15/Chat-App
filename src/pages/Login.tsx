4f2a5a25f6ab731ef2326ddc38cf9f0dc7e92595
davidxv15 <davidxvaz@gmail.com>
2024-10-30 00:10:41 -0700

    login css rule

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -100,7 +100,7 @@ const Login: React.FC = () => {
 
   return (
     <div
-      className="flex flex-col items-center justify-center min-h-screen bg-gray-900"
+      className="flex flex-col items-center justify-center min-h-screen bg-gray-900 full-screen-page"
       role="main"
       aria-labelledby="login-title"
     >

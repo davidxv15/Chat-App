b16728cb93d3fb2f7afef29644ce2ff05f12e82a
davidxv15 <davidxvaz@gmail.com>
2024-10-29 22:40:26 -0700

    DarkMode: styles

--- src/components/DarkModeToggle.tsx
+++ src/components/DarkModeToggle.tsx
@@ -30,9 +30,9 @@ const DarkModeToggle: React.FC = () => {
       className="dark-button bg-gray-700 dark:bg-gray-600 text-white dark:text-gray-300 px-2 py-2 rounded absolute top-12 right-4 z-50 hover:scale-110 transition-all duration-500 ease-in-out"
     >
       {darkMode ? (
-        <SunIcon className="h-5 w-5 dark:text-yellow-400" /> // Show Sun icon in DARK mode
+        <SunIcon className="h-5 w-5 dark:text-yellow-400" /> // Sun icon in DARK mode
       ) : (
-        <MoonIcon className="h-5 w-5 text-purple-400" /> // Show Moon icon in LIGHT mode
+        <MoonIcon className="h-5 w-5 text-purple-400" /> // Moon icon in LIGHT mode
       )}
     </button>
   );

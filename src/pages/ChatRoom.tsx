8690b65fd4e1b7d89c441b52f336953785f5dc1f
davidxv15 <davidxvaz@gmail.com>
2024-11-02 23:29:24 -0700

    ChatRoom: text color & style

--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -434,7 +434,7 @@ const ChatRoom: React.FC = () => {
       >
         <h1
           id="chat-title"
-          className="text-2xl text-gray-800 dark:text-gray-300 flex justify-center items-center"
+          className="font-bold text-2xl text-gray-800 dark:text-gray-400 flex justify-center items-center"
         >
           {" "}
           {roomName

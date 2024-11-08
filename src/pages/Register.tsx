35e093d1247819b33c4131a721380f58284f6daa
davidxv15 <davidxvaz@gmail.com>
2024-11-07 22:35:51 -0700

    Register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -88,7 +88,7 @@ const Register: React.FC = () => {
       aria-labelledby="register-title"
     >
       <h1 id="register-title" className="text-4xl text-gray-200 font-bold mb-4 italic-slant">
-        REGISTER
+        Register
       </h1>
 
       <form

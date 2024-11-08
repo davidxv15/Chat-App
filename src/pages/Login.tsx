e0d2e962683d6fcf92a3343083c9b359e425dfb9
davidxv15 <davidxvaz@gmail.com>
2024-11-07 22:35:07 -0700

    Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -105,7 +105,7 @@ const Login: React.FC = () => {
       aria-labelledby="login-title"
     >
       <h1 id="login-title" className="text-4xl font-bold mb-4 text-gray-200 italic-slant">
-        LOGIN
+        Login
       </h1>
 
       <form

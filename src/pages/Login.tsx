37e7d0fb1075ffdfc1fdffbfcab7b6a884707278
davidxv15 <davidxvaz@gmail.com>
2025-03-09 16:28:30 -0700

    refactor(log&reg):  /api placement

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -62,7 +62,7 @@ const Login: React.FC = () => {
     try {
       // Send recaptcha token to backend for verification
       const captchaResponse = await axios.post(
-        `${import.meta.env.VITE_API_URL}/verify-captcha`,
+        `${import.meta.env.VITE_API_URL}/api/verify-captcha`,
         { token }
       );
       if (captchaResponse.data.message === "Verification successful") {

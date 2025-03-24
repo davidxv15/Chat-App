bbaee00f98c30262d3576d446b085d3ea5e9c941
davidxv15 <davidxvaz@gmail.com>
2025-03-24 03:31:35 -0700

    refactor(RQA) quick login

--- src/components/RecruiterQuickAccess.tsx
+++ src/components/RecruiterQuickAccess.tsx
@@ -31,18 +31,18 @@ const RecruiterAccordion = () => {
         <div className="flex justify-between gap-4 text-sm font-mono text-gray-200">
           <div className="text-left border border-black p-2 rounded-md w-full">
             <p>
-              Username: <code className="text-green-200">kermit</code>
+              Username: <code className="text-red-200">mario</code>
             </p>
             <p>
-              Password: <code className="text-green-200">frog</code>
+              Password: <code className="text-red-200">brother</code>
             </p>
           </div>
           <div className="text-left border border-black p-2 rounded-md w-full">
             <p>
-              Username: <code className="text-pink-200">miss</code>
+              Username: <code className="text-green-200">luigi</code>
             </p>
             <p>
-              Password: <code className="text-pink-200">piggy</code>
+              Password: <code className="text-green-200">brother</code>
             </p>
           </div>
         </div>

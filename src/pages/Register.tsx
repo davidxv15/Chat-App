2cda9c68b2be4e096cb6ffb94b6428086706d6b3
davidxv15 <davidxvaz@gmail.com>
2025-03-23 22:36:26 -0700

    feat(Register): import recruiter access

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import axios from "axios";
+import RecruiterAccordion from "../components/RecruiterQuickAccess";
 
 declare global {
   interface Window {
